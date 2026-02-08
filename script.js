3de685e67bda0f6215c03ac0ce3c6a5f9c690f78
krishnakeerthanamitte-tech <krishnakeerthanamitte@gmail.com>
2026-02-08 16:34:23 +0530

    Stopwatch project

---
+++ script.js
@@ -0,0 +1,44 @@
+const display = document.getElementById("display");
+timer = null;
+startTime =0;
+elapsedTime =0;
+isRunning = false;
+function start(){
+    if(!isRunning){
+        startTime = Date.now()-elapsedTime;
+        timer = setInterval(update,10);
+        isRunning=true;
+    }
+   
+}
+function stop(){
+    if(isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now()-startTime;
+        isRunning = false;
+    }
+}
+function reset(){
+    clearInterval(timer);
+    startTime =0;
+    elapsedTime =0;
+    isRunning = false;
+    display.textContent = "00:00:00:00"
+}
+function update(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    hours =Math.floor(elapsedTime/(1000*60*60));
+    minutes = Math.floor(elapsedTime/(1000*60)%60);
+    seconds = Math.floor(elapsedTime/1000%60);
+    milliseconds = Math.floor(elapsedTime%1000/10);
+
+    hours = String(hours).padStart(2,"0");
+    minutes = String(minutes).padStart(2,"0");
+    seconds = String(seconds).padStart(2,"0");
+    milliseconds = String(milliseconds).padStart(2,"0");
+
+
+    display.textContent=`${hours}:${minutes}:${seconds}:${milliseconds}`
+}
